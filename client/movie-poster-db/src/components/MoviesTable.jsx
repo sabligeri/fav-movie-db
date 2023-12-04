bce527305e7ccee30c805d6fbf29b13e6facb1b0
Apolka Bercesi <139627888+B-Apolka@users.noreply.github.com>
2023-12-04 10:45:38 +0100

    Starter pack

---
+++ client/movie-poster-db/src/components/MoviesTable.jsx
@@ -0,0 +1,42 @@
+
+
+const MoviesTable = ({ movies }) => {
+  function handleSaveToFavMovies(movie) {
+    const poster = movie.Poster;
+    const title = movie.Title;
+    const year = movie.Year;
+    fetch('/api/favmovies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ poster, title, year })
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  return (
+    <table>
+        <tbody>
+          <tr>
+            {movies.map(movie => (
+              <th key={movie.imdbID}>
+                <img src={(movie.Poster !== 'N/A') ? movie.Poster : './src/noMoviePoster.jpg'} />
+                <h3> {movie.Title} </h3>
+                <h4> {movie.Year}</h4>
+                <button onClick={() => handleSaveToFavMovies(movie)}>Add to favorites</button>
+              </th>
+            )
+            )}
+          </tr>
+        </tbody>
+      </table>
+  )
+}
+
+export default MoviesTable;
---
+++ client/movie-poster-db/src/components/MyFavorites.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import DeleteVerify from "./DeleteVerify";
+
+function MyFavorites() {
+  const [seen, setSeen] = useState(null);
+  const [comment, setComment] = useState('');
+  const [favMovies, setFavMovies] = useState([]);
+  const [editFavMovies, setEditFavMovies] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [id, setId] = useState(null);
+
+  const deleteMovie = (id) => {
+    return fetch(`/api/favmovies/${id}`, { method: "DELETE" }).then((res) =>
+      res.json())
+      .then(setFavMovies((favMovies) => {
+        return favMovies.filter((favMovie) => favMovie._id !== id);
+      }))
+      .then(setShowModal(false))
+  };
+
+  useEffect(() => {
+    fetch('/api/favmovies')
+      .then(res => res.json())
+      .then(favMovies => setFavMovies(favMovies))
+      .catch(error => console.log(error))
+  }, [])
+
+
+  const handleEditFavMovies = (movie) => {
+    setEditFavMovies(movie);
+    setSeen(movie.seen);
+    setComment(movie.comment);
+  };
+
+  const handleUpdateFavMovies = () => {
+    if (editFavMovies) {
+      fetch(`/api/favmovies/${editFavMovies._id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          seen: seen,
+          comment: comment,
+        }),
+      })
+        .then(response => {
+          if (response.ok) {
+            setFavMovies(movies => movies.map(movie => movie._id === editFavMovies._id ? { ...movie, seen: seen, comment: comment }
+              : movie
+            )
+            );
+            setEditFavMovies(null);
+            setSeen('');
+            setComment('');
+          } else {
+            console.log('Failed to update favorite movie list!');
+          }
+        })
+        .catch(error => console.log(error));
+    }
+  };
+
+  return (
+    <>
+      <h1> My favorite movies </h1>
+    {(showModal === true) ? (
+        <DeleteVerify deleteMovie={() => deleteMovie(id)} closeModal={setShowModal}/>) : (null)
+      }
+      <div style={{ overflowX: "auto", display: "flex" }}>
+        {favMovies.map(movie => (
+          <div style={{ display: "inline-block", margin: "10px", textAlign: "center" }} key={movie._id}>
+            <img src={(movie.poster !== 'N/A') ? movie.poster : './src/noMoviePoster.jpg'} />
+            <h3>{movie.title}</h3>
+            <h4>{movie.year}</h4>
+            <div>
+              {editFavMovies === movie ? (
+                <label htmlFor="seenBox" style={{ display: "inline-block", }}>
+                Have you seen it?
+                <input
+                  type="checkbox"
+                  id="seenBox"
+                  checked={seen === 'seen'}
+                  onChange={() => setSeen(seen === 'seen' ? 'unseen' : 'seen')}
+                />
+              </label>
+              ) : (
+                movie.seen
+              )}
+            </div>
+            <div>
+              {editFavMovies === movie ? (
+                <textarea
+                  type="text"
+                  placeholder="Write your comment here"
+                  value={comment}
+                  rows={3}
+                  cols={30}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+              ) : (
+                movie.comment
+              )}
+            </div>
+            <div>
+              {editFavMovies === movie ? (
+                <button onClick={handleUpdateFavMovies}>Save</button>
+              ) : (
+                <button onClick={() => handleEditFavMovies(movie)}>Edit</button>
+              )}
+              <button onClick={() => { setShowModal(true); setId(movie._id) }}> Remove from favorites </button>
+            </div>
+          </div>
+        )
+        )}
+      </div>
+    </>
+  )
+}
+
+export default MyFavorites;
